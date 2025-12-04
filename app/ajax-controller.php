c30b028d9df242616463fa0f3528f691f27cbb06
Raj246297 <123247045+Raj246297@users.noreply.github.com>
2025-12-04 17:48:30 +0530

    Update ajax-controller.php

--- app/ajax-controller.php
+++ app/ajax-controller.php
@@ -31,6 +31,7 @@ if(!class_exists('CgGfAjaxController')){
                     }
                 }
             }
+            // test response
             wp_send_json(['status' => 200, 'message' => 'Success', 'options' => $options]);
             exit();
         }
