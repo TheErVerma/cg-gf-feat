e83472a490a760d251b95b8986b86b291f5585b3
Ashwani Verma <aswaniverma.codinggang@gmail.com>
2025-12-08 18:26:37 +0530

    Add inventory styles and enqueue CSS in admin
    Introduced new CSS styles for inventory components in style.css and enqueued the stylesheet for the WordPress admin using admin_enqueue_scripts. This improves the appearance of inventory tables and single inventory views in the plugin's admin interface.

--- cg-gf-feat.php
+++ cg-gf-feat.php
@@ -9,6 +9,8 @@
  * 
  */
 
+use MailPoetVendor\Doctrine\ORM\Query\Expr\Func;
+
 if (!defined('ABSPATH')) exit;
 
 define('CgGfFeat_VERSION', '2.0');
@@ -18,10 +20,10 @@ add_action('gform_loaded', array('CgGfFeat_BootUp', 'load'), 5);
 class CgGfFeat_BootUp
 {
 
+
     public static function load()
     {
-
-        if (! method_exists('GFForms', 'include_addon_framework')) {
+        if (!method_exists('GFForms', 'include_addon_framework')) {
             return;
         }
         require_once('app/ajax-controller.php');
@@ -34,9 +36,20 @@ class CgGfFeat_BootUp
         GFAddOn::register('CggffInventory');
         // GFAddOn::register('GF_Field_CgPhone');
     }
+
+    public function enqueue()
+    {
+        
+    }
 }
 
 function gf_simple_addon()
 {
     return CgGfFeat::get_instance();
 }
+
+
+
+add_action('admin_enqueue_scripts', function(){
+    wp_enqueue_style('cggffi-main', plugin_dir_url(__FILE__) . '/assets/css/style.css');
+});
